978cfb1a074cb76a46f0c14e8a8d1cb4d65d7536
Scott W. Morey <swmorey@gmail.com>
2013-05-20 08:39:58 -0400

    hellow world

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,23 @@
+class MainController < ApplicationController
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+
+  def index
+  
+if params[:q].present?
+@pagetitle = URI::escape('http://catalog.tadl.org/eg/opac/results?query=' + params[:q] + ';qtype=keyword;sort=pubdate.descending;')
+url = @pagetitle
+@doc = Nokogiri::HTML(open(url))
+   else
+  url = "http://catalog.tadl.org/eg/opac/results?qtype=keyword;sort=pubdate.descending;"
+@doc = Nokogiri::HTML(open(url))
+@pagetitle = @doc.at_css("title").text
+  end
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilscatcher::Application.config.secret_token = 'a76d636f8894f3810ee1ca92f74d0d7e40ddb4031dfcc1abb0946d0ba067d58a39b6a4e5ee178d579524e3bcb4365816d51b55869aefff19da3a21929fda7540'
